2e9fa30731c9197ca7767b5a474f2116a0fbe46e
adel saad <adelalderby@gmail.com>
2023-10-27 18:54:16 +0300

    build note item

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -7,7 +7,7 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.all(8.0),
+      padding: EdgeInsets.all(16.0),
       child: NotesViewBody(),
     );
   }
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -9,7 +9,72 @@ class NotesViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        NotesItem(),
       ],
+    );
+  }
+}
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      height: MediaQuery.of(context).size.height * 0.35,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: const Color(0xffF5C575),
+        borderRadius: BorderRadius.circular(22),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.black,
+                ),
+              ),
+              trailing: const Icon(
+                Icons.delete,
+                color: Colors.black,
+                size: 30,
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  'Build Your career with learning hot to code',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black.withOpacity(.70),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Row(
+              children: [
+                const Spacer(),
+                Text(
+                  'May 21,2022',
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black.withOpacity(.70),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
